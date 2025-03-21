07b9717f55190c95af4472dd198379adc88e1ffe
Divya Vishwakarma <vishwakarmad1999@gmail.com>
2025-03-21 15:20:30 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vishwakarmad1999/quotes
+
+go 1.24.1
---
+++ quotes.go
@@ -0,0 +1,9 @@
+package quotes
+
+func Sailor() string {
+	return "A smooth sea never made a skilled sailor."
+}
+
+func LifeChanges() string {
+	return "Changes are hard at first, messy in the middle, and beautiful at the end."
+}
