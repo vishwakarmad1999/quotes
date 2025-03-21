0e03f194b492a9dac7d6b9dfca209e379aff9a69
Divya Vishwakarma <vishwakarmad1999@gmail.com>
2025-03-21 15:42:26 +0530

    add BuildQuote function

--- quotes.go
+++ quotes.go
@@ -1,9 +1,23 @@
 package quotes
 
+import (
+	"errors"
+	"fmt"
+)
+
 func Sailor() string {
 	return "A smooth sea never made a skilled sailor."
 }
 
 func LifeChanges() string {
 	return "Changes are hard at first, messy in the middle, and beautiful at the end."
+}
+
+func BuildQuote(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("please pass your name as the argument to quotes.BuildQuote")
+	}
+
+	quote := fmt.Sprintf("%v, never settle.", name)
+	return quote, nil
 }
